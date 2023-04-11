2f349e6652e94132ccd520488a87ee082af03f99
yerma4ok <yerma4ok13@gmail.com>
2023-04-12 00:21:53 +0200

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+
+const DB_HOST = "mongodb+srv://yerma4ok:vk069231@cluster0.fvkwbxd.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+.then(()=> app.listen(3000))
+.catch(error => console.log(error.message));
+
